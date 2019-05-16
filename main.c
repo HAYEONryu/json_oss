d8590fb2eb4e67d5b7daed2556eec5d92b54bea5
KiwonMoon <49269128+KiwonMoon@users.noreply.github.com>
2019-05-16 17:02:51 +0900

    Add files via upload

---
+++ main.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+//#include <json/json.h>
+ //#include "parson.h"
+
+ int main() {
+     
+     FILE *fp = fopen(filename, "file");
+     if (fp == NULL) {
+         return NULL;
+     }
+
+     int size;
+     char *buffer;
+
+     fseek(fp, 0, SEEK_END);
+     size = ftell(fp);
+     fseek(fp, 0, SEEK_SET);
+
+     buffer = malloc(size + 1);
+     memset(buffer, 0, size + 1);
+
+     if (fread(buffer, size, 1, fp) < 1) {
+         *readSize = 0;
+         free(buffer);
+         fclose(fp);
+         return NULL;
+     }
+
+     *readSize = size;
+
+     fclose(fp);
+
+     return buffer;
+ }
