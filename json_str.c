e0b93e10102517b3c4887da1a5a2893e74f07248
KiwonMoon <49269128+KiwonMoon@users.noreply.github.com>
2019-05-23 16:57:24 +0900

    Add files via upload
    json stucture

--- json_str.c
+++ json_str.c
@@ -1,22 +1,6 @@
 #include <stdio.h>
 #define TOKEN_COUNT 1024
 
-//token structure
-typedef struct {
-    type_t type;
-    int start;
-    int end;
-    int size;
-} tok_t;
-
-typedef enum {
-    UNDEFINED = 0,
-    OBJECT = 1,
-    ARRAY = 2,
-    STRING = 3,
-    PRIMITIVE = 4
-} type_t;
-
 typedef sturct {
     tok_t tokens[TOKEN_COUNT];
 }JSON;
