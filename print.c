2fa88e20f7a97954ce192f8a51170cc40fb7d7c0
HAYEONryu <44168356+HAYEONryu@users.noreply.github.com>
2019-06-02 08:54:42 +0900

    Update print.c

--- print.c
+++ print.c
@@ -5,6 +5,27 @@ void print(char *text, JSON *json, int howmany){
         for(int j = json->tokens[i].start; j<= json->tokens[i].end; j++){
             printf("%c", text[j]);
         }
-        printf(" (size=%d, %d~%d, typeshouldbehere) \n", json->tokens[i].size, json->tokens[i].start, json->tokens[i].end);
+        printf("  (size=%d, %d~%d, ", json->tokens[i].size, json->tokens[i].start, json->tokens[i].end);
+		switch (json->tokens[i].type)
+		{
+		case STRING:
+			printf("JSNM_STRING)\n");
+			break;
+		case OBJECT:
+			printf("JSNM_OBJECT)\n");
+			break;
+		case PRIMITIVE:
+			printf("JSNM_PRIMITIVE)\n");
+			break;
+		case ARRAY:
+			printf("JSNM_ARRAY)\n");
+			break;
+		case UNDEFINED:
+			printf("JSNM_UNDEFINED)\n");
+			break;
+		default:
+			printf("ERROR)\n");
+			break;
+		}
     }        
 }
