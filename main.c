308284b02ae1b0748f818696c74aa7e707052699
HAYEONryu <44168356+HAYEONryu@users.noreply.github.com>
2019-06-03 05:08:35 +0900

    Update main.c

--- main.c
+++ main.c
@@ -6,17 +6,12 @@ int main(int argc, char *argv[]) {
 
 	char *text;
 	int howmany = 0;
-	printf("1\n");
 	text = readFile(argv[1], &size);
 	//char* readFile(argv[1], &size);
-	printf("2\n");
 
 	JSON json = { 0, };
 	parser(text, size, &json, &howmany);
-	printf("3\n");
 	print(text, &json, howmany);
-	printf("4\n");
 	return 0;
 }
-
 
